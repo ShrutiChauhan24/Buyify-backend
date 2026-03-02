f852c8915af8de34f8a01c362812afead4cca339
Shruti <sanjitchauhan165@gmail.com>
2026-03-02 11:04:00 +0530

    Removed express-session, added JWT authentication support

--- index.js
+++ index.js
@@ -25,16 +25,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-app.use(
-  session({
-    secret: "keyboardcat",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/api',authRoute);
 app.use('/api',categoryRoute);
